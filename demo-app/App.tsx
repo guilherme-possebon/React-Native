e1e08e34c5d175db374995c26e03cceec1830a0f
Guilherme Possebon <gpossebon67@gmail.com>
2024-11-06 22:13:42 -0300

    Primeiro app criado em react native

--- demo-app/App.tsx
+++ demo-app/App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Contador: {count}</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(count + 10)}
+      >
+        <Text style={styles.textButton}>Incrementar</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "#f00",
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#3f51b5",
+    padding: 16,
+    borderRadius: 8,
+  },
+  textButton: {
+    color: "#fff",
+    fontSize: 16,
   },
 });
