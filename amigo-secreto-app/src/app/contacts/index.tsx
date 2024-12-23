f7d4536f0c7601fd169ffddfa90935f2ced46fc0
Guilherme Possebon <gpossebon67@gmail.com>
2024-12-23 10:10:56 -0300

    adição de autenticação por biometria

--- amigo-secreto-app/src/app/contacts/index.tsx
+++ amigo-secreto-app/src/app/contacts/index.tsx
@@ -1,119 +1,126 @@
-import { Alert, FlatList, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { colors, theme } from "../../themes/global";
 import { useContext, useEffect, useState } from "react";
 import { IContact } from "../../@types/contact";
 import { Icon } from "../../components/Icon";
 
-import styles from './styles'
+import styles from "./styles";
 import { AppContext } from "../../contexts/AppContext";
 
 //com o expo-router, todas as telas precisam retornar DEFAULT
 export default function Contacts() {
+  const { contactsList, storeData } = useContext(AppContext);
+  const [contact, setContact] = useState<IContact>({} as IContact);
 
-    const { contactsList, storeData } = useContext(AppContext);
-    const [contact, setContact] = useState<IContact>({} as IContact);
-    
-    const save = () => {
+  const save = () => {
+    let maxId = 0;
+    contactsList.map((item) => {
+      if (item.id > maxId) {
+        maxId = item.id;
+      }
+    });
 
-        let maxId = 0;
-        contactsList.map(item => {
-            if (item.id > maxId) {
-                maxId = item.id;
-            }
-        })
-        
-        const newList = [...contactsList,
+    const newList = [
+      ...contactsList,
+      {
+        id: maxId + 1,
+        name: contact.name,
+        number: contact.number,
+      },
+    ];
+
+    storeData(newList);
+    setContact({
+      name: "",
+      number: "",
+    } as IContact); //Limpar o objeto de contato utilizado
+  };
+
+  const removeItem = (id: number) => {
+    try {
+      Alert.alert("Remover Item", "Tem certeza disso?", [
         {
-            id: maxId + 1,
-            name: contact.name,
-            number: contact.number
-        }
-        ];
+          text: "Cancelar",
+          onPress: () => {
+            console.log("Operação cancelada");
+          },
+        },
+        {
+          text: "Sim",
+          onPress: () => {
+            const newList = contactsList.filter((item) => item.id != id);
 
-        storeData(newList);
-        setContact({
-            name: '',
-            number: ''
-        } as IContact) //Limpar o objeto de contato utilizado
+            storeData(newList);
+          },
+        },
+      ]);
+    } catch (err) {
+      console.log("🚀 ~ removeItem ~ err:", err);
     }
+  };
 
-    const removeItem = (id: number) => {
-        try {
-            Alert.alert('Remover Item', 'Tem certeza disso?', [
-                {
-                    text: 'Cancelar',
-                    onPress: () => {
-                        console.log('Operação cancelada');
-                    }
-                },
-                {
-                    text: 'Sim',
-                    onPress: () => {
+  const Item = ({ id, name, number }: IContact) => (
+    <View style={styles.item}>
+      <Text style={styles.contact}>
+        {name} - {number}
+      </Text>
+      <TouchableOpacity onPress={() => removeItem(id)}>
+        <Icon name="trash" size={18} color={colors.red} />
+      </TouchableOpacity>
+    </View>
+  );
 
-                        const newList = contactsList.filter(item => item.id != id);
+  return (
+    <SafeAreaView style={theme.container}>
+      <View style={styles.form}>
+        <TextInput
+          style={[theme.input, { width: "40%" }]}
+          onChangeText={(value) =>
+            setContact({
+              ...contact,
+              name: value,
+            })
+          }
+          placeholder="Nome"
+          autoCapitalize="characters"
+          value={contact.name}
+        />
 
-                        storeData(newList);
-                    }
-                }
-            ])
-        } catch (err) {
-            console.log("🚀 ~ removeItem ~ err:", err)
-        }
-    }
+        <TextInput
+          style={[theme.input, { width: "40%" }]}
+          onChangeText={(value) =>
+            setContact({
+              ...contact,
+              number: value,
+            })
+          }
+          placeholder="Telefone"
+          value={contact.number}
+        />
 
-    const Item = ({ id, name, number }: IContact) => (
-        <View style={styles.item}>
-            <Text style={styles.contact}>{name} - {number}</Text>
-            <TouchableOpacity
-                onPress={() => removeItem(id)}>
-                <Icon name='trash' size={18} color={colors.red} />
-            </TouchableOpacity>
-        </View>
-    );
+        <TouchableOpacity onPress={() => save()}>
+          <Icon name="save" size={38} color={colors.primary} />
+        </TouchableOpacity>
+      </View>
 
-    return (
-        <SafeAreaView style={theme.container}>
+      <Text style={[theme.title, theme.marginTop]}>Lista de Contatos:</Text>
 
-            <View style={styles.form}>
-                <TextInput
-                    style={theme.input}
-                    onChangeText={(value) => setContact({
-                        ...contact,
-                        name: value
-                    })}
-                    placeholder="Nome"
-                    autoCapitalize="characters"
-                    value={contact.name}
-                />
-
-                <TextInput
-                    style={theme.input}
-                    onChangeText={(value) => setContact({
-                        ...contact,
-                        number: value
-                    })}
-                    placeholder="Telefone"
-                    value={contact.number}
-                />
-
-                <TouchableOpacity
-                    onPress={() => save()}>
-                    <Icon name='save' size={38} color={colors.primary} />
-                </TouchableOpacity>
-            </View>
-
-
-            <Text style={[theme.title, theme.marginTop]}>Lista de Contatos:</Text>
-
-            <FlatList
-                data={contactsList}
-                renderItem={({ item }) =>
-                    <Item id={item.id} name={item.name} number={item.number} />
-                }
-                ListEmptyComponent={<Text style={styles.placeholder}>Lista Vazia</Text>}
-                keyExtractor={item => item.id.toString()}
-            />
-
-        </SafeAreaView>
-    )
+      <FlatList
+        data={contactsList}
+        renderItem={({ item }) => (
+          <Item id={item.id} name={item.name} number={item.number} />
+        )}
+        ListEmptyComponent={<Text style={styles.placeholder}>Lista Vazia</Text>}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </SafeAreaView>
+  );
 }
--- amigo-secreto-app/src/app/index.tsx
+++ amigo-secreto-app/src/app/index.tsx
@@ -1,89 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { theme } from '../themes/global';
-import { IUser } from '../@types/user';
-import axios from 'axios';
-import { router } from 'expo-router';
-import * as LocalAuthentication from 'expo-local-authentication';
+import React, { useEffect, useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { theme } from "../themes/global";
+import { IUser } from "../@types/user";
+import axios from "axios";
+import { router } from "expo-router";
+import * as LocalAuthentication from "expo-local-authentication";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function login() {
-
   const [user, setUser] = useState<IUser>({} as IUser);
 
-  const handleLogin = async () => {
+  const handleLogin = async (usuario: string, senha: string) => {
     try {
-
       if (user.user && user.pass) {
-
         const authInfo = btoa(`${user.user}:${user.pass}`);
 
         //passar a autenticação
         const options = {
           headers: {
-            'Authorization': `Basic ${authInfo}`
-          }
-        }
+            Authorization: `Basic ${authInfo}`,
+          },
+        };
 
-        const { status } = await axios.get('/getUsersList', options);
+        const { status } = await axios.get("/getUsersList", options);
         if (status === 200) {
-          console.log('STATUS => ', status);
-          router.replace('home');
+          console.log("STATUS => ", status);
+
+          await AsyncStorage.setItem("user", user.user);
+          await AsyncStorage.setItem("pass", user.pass);
+
+          router.replace("home");
         }
-
       } else {
-        Alert.alert('Atenção', 'Informe usuário e senha');
+        Alert.alert("Atenção", "Informe usuário e senha");
       }
-
     } catch (err) {
-      console.log('ERR => ', err);
+      console.log("ERR => ", err);
     }
-  }
+  };
 
   const getLocalAuth = async () => {
-    const result = LocalAuthentication.authenticateAsync();
-  }
+    const savedBiometrics = await LocalAuthentication.isEnrolledAsync();
+    if (savedBiometrics) {
+      const biometricAuth = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Login com biometria",
+        disableDeviceFallback: true,
+        cancelLabel: "Cancelar",
+        requireConfirmation: true,
+      });
+
+      if (biometricAuth.success) {
+      }
+    }
+  };
 
   useEffect(() => {
-
     getLocalAuth();
-
-  }, [])
+  }, []);
 
   return (
     <View style={theme.container}>
-
       <View style={styles.form}>
-        <TextInput
-          style={theme.input}
-          placeholder='Usuário'
-          autoCapitalize='none'
-          value={user.user}
-          onChangeText={value => setUser({ ...user, user: value })}
-        />
+        <View style={{ width: "100%", gap: 16 }}>
+          <TextInput
+            style={theme.input}
+            placeholder="Usuário"
+            autoCapitalize="none"
+            value={user.user}
+            onChangeText={(value) => setUser({ ...user, user: value })}
+          />
 
-        <TextInput
-          style={theme.input}
-          placeholder='Senha'
-          autoCapitalize='none'
-          secureTextEntry
-          value={user.pass}
-          onChangeText={value => setUser({ ...user, pass: value })}
-        />
+          <TextInput
+            style={theme.input}
+            placeholder="Senha"
+            autoCapitalize="none"
+            secureTextEntry
+            value={user.pass}
+            onChangeText={(value) => setUser({ ...user, pass: value })}
+          />
+        </View>
 
-        <TouchableOpacity
-          onPress={() => handleLogin()}>
-          <Text>LOGIN</Text>
+        <TouchableOpacity onPress={() => handleLogin()} style={theme.button}>
+          <Text style={theme.textButton}>LOGIN</Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   form: {
-    width: '100%',
-    flexDirection: 'column',
-    paddingHorizontal: 16
-  }
-})
+    width: "100%",
+    flexDirection: "column",
+    paddingHorizontal: 16,
+    alignItems: "center",
+    gap: 16,
+  },
+});
